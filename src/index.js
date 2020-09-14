a108e8de26a9b30620a2cbcb6a6a7a6f256b89a1
Sunil Chauhan <sunil.ch96@gmail.com>
2020-09-14 17:53:50 +0530

    1# React Elements
    1# React Elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//React Element 1
+
+//const element1 = <h1>Welcome To codebaseline tutorials..</h1>;
+//ReactDOM.render(element1, document.getElementById("root"));
+
+//const element1 = <h1>A simple header text</h1>;
+//ReactDOM.render(element2, document.getElementById("root2"));
+
+// const element3 = (
+//   <div>
+//     <h1>Welcome To codebaseline tutorials..</h1>
+//     <h1>A simple header text</h1>
+//   </div>
+// );
+// ReactDOM.render(element3, document.getElementById("root"));
+
+//React Element 2
+const reactElement = React.createElement(
+  "div",
+  null,
+  React.createElement("h1", null, "This elements is created by react."),
+  React.createElement("h1", null, "Second Div.")
 );
+ReactDOM.render(reactElement, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
