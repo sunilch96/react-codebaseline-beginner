04ed8991b2b0259bbe78a739ef9582716f6af00f
Sunil Chauhan <sunil.ch96@gmail.com>
2020-09-18 10:57:39 +0530

    5# Class Components
    5# Class Components

--- src/index.js
+++ src/index.js
@@ -1,33 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
-//#4: React Elemets
-function DisplayEmpInfo(employee) {
-  return (<div>
-    <p><label>Employee ID: <b>{employee.id}</b></label> </p>
-    <p> <label>Employee Name: <b>{employee.name}</b> </label> </p>
-    <p> <label> Employee Location: <b>{employee.location}</b></label></p>
-    <p><label>Employee Salary: <b>{employee.salary}</b></label></p>
-    <br></br>
-    <DeptInfo deptname={employee.deptname} depthead={employee.depthead}></DeptInfo>
-  </div>
-  );
+//#5 Class Components
+export class DisplayEmpInfo extends Component {
+  constructor(props) {
+    super(this.props)
+    console.log(this.props);
+  }
+  render() {
+    return <div>
+      <p><label>Employee ID: <b>{this.props.id}</b></label> </p>
+      <p> <label>Employee Name: <b>{this.props.name}</b> </label> </p>
+      <p> <label> Employee Location: <b>{this.props.location}</b></label></p>
+      <p><label>Employee Salary: <b>{this.props.salary}</b></label></p>
+      <br></br>
+      <DeptInfo deptname={this.props.deptname} depthead={this.props.depthead}></DeptInfo>
+    </div>
+  };
 }
-const DeptInfo = (dept) => {
-  return <div>
-    <p>Dept Name:<b>{dept.deptname}</b></p>
-    <p>Dept Head:<b>{dept.depthead}</b></p>
-  </div>
+export class DeptInfo extends Component {
+  render() {
+    return <div>
+      <p>Dept Name:<b>{this.props.deptname}</b></p>
+      <p>Dept Head:<b>{this.props.depthead}</b></p>
+    </div>
+  }
 }
 const empElement = (<DisplayEmpInfo id="1" name="codebaseline" location="Pune" salary="34570"
-  deptname="Computers" depthead="Alex">
-
-</DisplayEmpInfo>
+  deptname="Computers" depthead="Alex"></DisplayEmpInfo>
 );
 ReactDOM.render(empElement, document.getElementById("root"));
+
+
+//#4: React Elemets
+// function DisplayEmpInfo(employee) {
+//   return (<div>
+//     <p><label>Employee ID: <b>{employee.id}</b></label> </p>
+//     <p> <label>Employee Name: <b>{employee.name}</b> </label> </p>
+//     <p> <label> Employee Location: <b>{employee.location}</b></label></p>
+//     <p><label>Employee Salary: <b>{employee.salary}</b></label></p>
+//     <br></br>
+//     <DeptInfo deptname={employee.deptname} depthead={employee.depthead}></DeptInfo>
+//   </div>
+//   );
+// }
+// const DeptInfo = (dept) => {
+//   return <div>
+//     <p>Dept Name:<b>{dept.deptname}</b></p>
+//     <p>Dept Head:<b>{dept.depthead}</b></p>
+//   </div>
+// }
+// const empElement = (<DisplayEmpInfo id="1" name="codebaseline" location="Pune" salary="34570"
+//   deptname="Computers" depthead="Alex">
+
+// </DisplayEmpInfo>
+// );
+// ReactDOM.render(empElement, document.getElementById("root"));
+//end #4
+
 
 //React Element 1
 
